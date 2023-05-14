5e31622f552b2cb442b07b8a15ac01f81e1d704f
Codehash001 <codehashblockchain@gmail.com>
2023-05-14 09:58:41 +0530

    css fix

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ const ArrowClickedHnadler= () => {
         <link rel="icon" href="/favicon.png" />
       </Head>
       
-        <div className="min-h-screen bg-cover bg- bg-center md:px-16 px-4 md:py-8 pt-4 flex flex-col items-center md:justify-start justify-center">
+        <div className="min-h-autto bg-cover bg- bg-center md:px-16 px-4 md:py-8 py-4 flex flex-col items-center md:justify-start justify-center">
         <img src='/ThePepeArmy/thepepearmy-logo.png' className="md:w-[250px] w-[180px] h-auto mb-5"/>
         {/* <h1 className="text-white text-lg md:mb-[100px] font-semibold"> Welcome to a web3 experience</h1> */}
           <div className="flex flex-col items-center">
