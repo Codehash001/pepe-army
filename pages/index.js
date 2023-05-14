0e4b2833b208c90ece105b7406e57805c11e1668
Codehash001 <codehashblockchain@gmail.com>
2023-05-14 13:27:52 +0530

    bg fix

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ const ArrowClickedHnadler= () => {
         <link rel="icon" href="/favicon.png" />
       </Head>
       
-        <div className="min-h-autto bg-cover bg- bg-center md:px-16 px-4 md:py-8 py-4 flex flex-col items-center md:justify-start justify-center">
+        <div className="min-h-autto bg-cover bg-[#509a44] bg-center md:px-16 px-4 md:py-8 py-4 flex flex-col items-center md:justify-start justify-center">
         <img src='/ThePepeArmy/thepepearmy-logo.png' className="md:w-[250px] w-[180px] h-auto mb-5"/>
         {/* <h1 className="text-white text-lg md:mb-[100px] font-semibold"> Welcome to a web3 experience</h1> */}
           <div className="flex flex-col items-center">
@@ -39,8 +39,8 @@ const ArrowClickedHnadler= () => {
         <Midcontent/>
         {/* <img src='/TurboToads/turbotads-character-banner-2.png' className="w-screen h-auto"/> */}
         <FAQ/>
-        <img className='w-auto min-h-screen  md:mb-0 font-Kanit fixed bottom-0 -z-10'
-      src="/ThePepeArmy/thepepearmy-header-bg.png"/>
+        {/* <img className='w-auto min-h-screen  md:mb-0 font-Kanit fixed bottom-0 -z-10'
+      src="/ThePepeArmy/thepepearmy-header-bg.png"/> */}
     </>
   );
 }
